76b0f13655580f57e782b8e7b664b6419d1fdf0a
RMadya <kereyoben75@gmail.com>
2024-05-20 11:08:04 +0700

    deklarasi fungsi dan variable class mahasiswa private

--- ParadigmaOOP1_0143/ParadigmaOOP1_0143.cpp
+++ ParadigmaOOP1_0143/ParadigmaOOP1_0143.cpp
@@ -2,27 +2,11 @@
 #include <string>
 using namespace std;
 
-class mahasiswa {
-private:
-	const int id;
-	string nama;
-	float nilai;
-
-public:
-	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
-
-	}
-	~mahasiswa() {
-		cout << "Id    = " << id << endl;
-		cout << "Nama  = " << nama << endl;
-		cout << "Nilai = " << nilai << endl;
-	}
-};
+#include "JANTUNG.h"
+#include "MANUSIA.h"
 
 int main() {
-	mahasiswa mhs(12, "Asroni", 90.5);
+	manusia* varManusia = new manusia("jono");
+	delete varManusia;
 	return 0;
 }
-
-	
-
